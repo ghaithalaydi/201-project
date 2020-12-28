f93b97697e970b0f767130a3ed6b497028477be2
ghaith alaydi <ghaith.alaydi@gmail.com>
2020-12-28 13:35:47 +0200

    Added the project per the req

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ alert("Welcome to the website" + userName + ", Lets get to know each other a lit
 
 var firstAnswer = prompt("Do you think that I am an Engineer? (yes / no answers only)");
 // console.log(firstAnswer);
-alert("Your answer was: "+firstAnswer);
+alert("Your answer was: " + firstAnswer);
 switch (firstAnswer.toLowerCase()) {
 
     case "yes":
@@ -18,7 +18,7 @@ switch (firstAnswer.toLowerCase()) {
 
 var secondAnswer = prompt("Is my prefered field of work related to consultancy work? (yes / no answers only)");
 // console.log(secondAnswer);
-alert("Your answer was: "+secondAnswer);
+alert("Your answer was: " + secondAnswer);
 switch (secondAnswer.toLowerCase()) {
 
     case "yes":
@@ -32,7 +32,7 @@ switch (secondAnswer.toLowerCase()) {
 
 var thirdAnswer = prompt("Do I have over 7 years of work experience? (yes / no answers only)");
 // console.log(thirdAnswer);
-alert("Your answer was: "+thirdAnswer);
+alert("Your answer was: " + thirdAnswer);
 switch (thirdAnswer.toLowerCase()) {
 
     case "no":
@@ -46,7 +46,7 @@ switch (thirdAnswer.toLowerCase()) {
 
 var fourthAnswer = prompt("Do I speak any other languages apart from english and arabic? (yes / no answers only)");
 // console.log(fourthAnswer);
-alert("Your answer was: "+ fourthAnswer);
+alert("Your answer was: " + fourthAnswer);
 switch (fourthAnswer.toLowerCase()) {
 
     case "yes":
@@ -60,7 +60,7 @@ switch (fourthAnswer.toLowerCase()) {
 
 var fifthAnswer = prompt("Do you think i prefer smaller projects to bigger ones? (yes / no answers only)");
 // console.log(fifthAnswer);
-alert("Your answer was: "+fifthAnswer);
+alert("Your answer was: " + fifthAnswer);
 switch (fifthAnswer.toLowerCase()) {
 
     case "no":
@@ -72,4 +72,4 @@ switch (fifthAnswer.toLowerCase()) {
         break;
 }
 
-alert("Well "+ userName +" it was a pleasure!");
+alert("Well " + userName + " it was a pleasure!");
